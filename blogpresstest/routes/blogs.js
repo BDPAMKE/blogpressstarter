1ffe108cd6aa590fea4a327ec6f7690d18bde6ad
kempensp <kempensp@milwaukee.k12.wi.us>
2023-06-03 17:38:54 +0000

    Added support for markdown

--- blogpresstest/routes/blogs.js
+++ blogpresstest/routes/blogs.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var MarkdownIt = require('markdown-it'),
+md = new MarkdownIt();
 
 /* GET home page. */
 router.get('/:blogName', function(req, res, next) {
-  res.render('index', { title: req.params.blogName });
+  var content=('# I want to see if this can work');
+  content=content+("\n ## testing another line");
+  content+=("\n Try going to the [CNN home page](http://www.cnn.com)")
+  var result = md.render(content);
+  res.render('blogpage', { title: req.params.blogName, content: result });
 });
-// Get subpage
+
 router.get('/:blogName/:pageName', function(req, res, next) {
-    res.render('index', { title: req.params.blogName + ' ' +req.params.pageName });
+    var result = md.render('## markdown-it rulezz!');
+    res.render('blogpage', { title: req.params.blogName + ' ' +req.params.pageName , content: result});
   });
 
 module.exports = router;
