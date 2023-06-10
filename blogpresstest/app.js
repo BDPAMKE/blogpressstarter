4e9d51c516d2bc57fb45f10146b77c1c85c41e89
Amina <100482564+ItsAminah@users.noreply.github.com>
2023-06-10 17:05:05 +0000

    loginupdate06/10

--- blogpresstest/app.js
+++ blogpresstest/app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var blogsRouter = require('./routes/blogs');
-
+var loginRouter = require('./routes/login')
 var app = express();
 
 
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/blogs', blogsRouter);
+app.use('/login', loginRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   res.status(404).render('error', {title:'404 page not found'});
